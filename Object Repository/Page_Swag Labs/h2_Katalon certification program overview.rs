d0588472d24c4cfce3642e77c1e139856a1333d4
Admin <Admin@DEOLINDO.home>
2023-02-02 22:49:33 +0100

    Text rename

---
+++ Object Repository/Page_Swag Labs/h2_Katalon certification program overview.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_Katalon certification program overview</name>
+   <tag></tag>
+   <elementGuidId>9da048f4-b5b9-46ac-9461-44cf9f918b2b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='page']/main/section[3]/div/h2</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.program__container.qubely-container > h2.section__title</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+      <webElementGuid>a0b34ab7-d85d-4821-b6cd-f44fc734fca3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>section__title</value>
+      <webElementGuid>8ca5aba2-420f-4962-b770-04aa10174068</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Katalon certification program overview</value>
+      <webElementGuid>4425f8b4-912d-45cf-a18f-6fdbb1c8730d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;page&quot;)/main[@class=&quot;cert qubely-section&quot;]/section[@class=&quot;program&quot;]/div[@class=&quot;program__container qubely-container&quot;]/h2[@class=&quot;section__title&quot;]</value>
+      <webElementGuid>fbef145a-6f8f-4041-994e-b43e572a73f5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='page']/main/section[3]/div/h2</value>
+      <webElementGuid>5ff4958f-cf47-4b5d-8973-e77fd2eb3e8a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Learn whenever it best suits you since Katalon Academy courses are online and self-paced.'])[1]/following::h2[1]</value>
+      <webElementGuid>8b102ddd-af43-4764-9559-4ff1eeda96cb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Learn for free at your own pace'])[1]/following::h2[1]</value>
+      <webElementGuid>fb14be19-1ee4-4e58-8b99-3484e185e313</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Practitioner'])[1]/preceding::h2[1]</value>
+      <webElementGuid>a667f6ad-69c4-4aa0-be01-bcec213ea979</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Requirements:'])[1]/preceding::h2[1]</value>
+      <webElementGuid>0b09e49b-e4e0-4058-a013-eee694d12f2e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Katalon certification program overview']/parent::*</value>
+      <webElementGuid>0166ffdd-e7ca-4603-be23-e7b4e034dbc1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section[3]/div/h2</value>
+      <webElementGuid>203ee8ec-95ee-4cc1-a13d-8843428a3189</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h2[(text() = 'Katalon certification program overview' or . = 'Katalon certification program overview')]</value>
+      <webElementGuid>d722f5af-af67-41fe-876b-66dc75e1afa0</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Swag Labs/h3_Katalon Certification Program.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Katalon Certification Program</name>
+   <tag></tag>
+   <elementGuidId>8fe06376-97cf-4e79-bb1f-184a21464de0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='rso']/div/div/div/div/div/div/div/div/div/a/h3</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h3.LC20lb.MBeuO.DKV0Md</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+      <webElementGuid>719e7d47-9e28-40bb-994d-a0da2c0da35d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>LC20lb MBeuO DKV0Md</value>
+      <webElementGuid>1b3f8a28-9d38-4f45-9768-eaa1a19422be</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Katalon Certification Program</value>
+      <webElementGuid>62de3381-8150-47b1-bc75-a38e4dcade72</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;rso&quot;)/div[@class=&quot;hlcw0c&quot;]/div[@class=&quot;MjjYud&quot;]/div[1]/div[@class=&quot;g&quot;]/div[1]/div[1]/div[@class=&quot;kvH3mc BToiNc UK95Uc&quot;]/div[@class=&quot;Z26q7c UK95Uc jGGQ5e&quot;]/div[@class=&quot;yuRUbf&quot;]/a[1]/h3[@class=&quot;LC20lb MBeuO DKV0Md&quot;]</value>
+      <webElementGuid>576e237b-4b0a-4e70-a12d-54ce3b65fd38</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='rso']/div/div/div/div/div/div/div/div/div/a/h3</value>
+      <webElementGuid>5d7f9fef-d235-4964-a47a-c887e2071745</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Resultados da procura'])[1]/following::h3[1]</value>
+      <webElementGuid>c8013616-7f4d-4f06-b795-6873aa017b1d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Pesquisar na ajuda'])[1]/following::h3[1]</value>
+      <webElementGuid>a407e301-546b-4c3b-99e9-08c28256ea5b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='› certi...'])[1]/preceding::h3[1]</value>
+      <webElementGuid>b762ed84-54af-4c60-a72a-8c4d6bc04146</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Katalon Certification Program']/parent::*</value>
+      <webElementGuid>3ad07450-82c0-4531-b6c9-1336cacc9cbe</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h3</value>
+      <webElementGuid>d1784dfc-6fcf-459f-b870-85e839d28fc1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[(text() = 'Katalon Certification Program' or . = 'Katalon Certification Program')]</value>
+      <webElementGuid>5551c2e9-a5e8-4384-8fc2-52e189e0d2e6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
